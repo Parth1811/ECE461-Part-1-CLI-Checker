83d6366179df1913ff2b8cc0156ce83811a57039
Parth Patil <parthvin@gmail.com>
2024-09-03 12:11:13 -0400

    Fixed the logging in checker

--- checker.py
+++ checker.py
@@ -25,9 +25,9 @@ def run_install() -> int:
     url_file_rc, output = url_file.run()
     total_correct = install_rc + test_suite_rc + url_file_rc
 
-    print_test_result("Install command %s successfully!", install_rc, "exited", "did not exit")
-    print_test_result("Subsequent test command %s successfully!", test_suite_rc, "exited", "did not exit")
-    print_test_result("Subsequent URL_FILE command %s successfully!", url_file_rc, "exited", "did not exit")
+    print_test_result("> Install command %s successfully!", install_rc, "exited", "did not exit")
+    print_test_result("> Subsequent test command %s successfully!", test_suite_rc, "exited", "did not exit")
+    print_test_result("> Subsequent URL_FILE command %s successfully!", url_file_rc, "exited", "did not exit")
 
     return total_correct
 
@@ -45,7 +45,7 @@ def run_urlfile() -> int:
     url_file_rc, output = url_file.run()
     total_correct = 0
 
-    print_test_result("URL_FILE command %s successfully!", url_file_rc)
+    print_test_result("> URL_FILE command %s successfully!", url_file_rc)
     if url_file_rc:
         total_correct += 1
     else:
@@ -60,21 +60,21 @@ def run_urlfile() -> int:
     except Exception as e:
         pass
 
-    print_test_result("URL_FILE output is %s NDJSON!", is_valid_output, "valid", "not valid")
+    print_test_result("> URL_FILE output is %s NDJSON!", is_valid_output, "valid", "not valid")
     if is_valid_output:
         total_correct += 1
     else:
         return total_correct
     
     module_score = MODULE_SCORE(output)
-    print_test_result("URL_FILE output is a %s module score!", module_score.is_valid(), "valid", "not valid")
+    print_test_result("> URL_FILE output is a %s module score!", module_score.is_valid(), "valid", "not valid")
     if module_score.is_valid():
         total_correct += 1
     
     os.environ["LOG_FILE"] = ""
     url_file = CLI_CMD_WRAPPER("./run one-url.txt")
     url_file_rc, output = url_file.run()
-    print_test_result("URL_FILE command %s successfully when LOG_FILE is not set!", not url_file_rc, "did not exit", "exited")
+    print_test_result("> URL_FILE command %s successfully when LOG_FILE is not set!", not url_file_rc, "did not exit", "exited")
     if not url_file_rc:
         total_correct += 1
 
@@ -82,7 +82,7 @@ def run_urlfile() -> int:
     os.environ["GITHUB_TOKEN"] = ""
     url_file = CLI_CMD_WRAPPER("./run one-url.txt")
     url_file_rc, output = url_file.run()
-    print_test_result("URL_FILE command %s successfully when GITHUB_TOKEN is not set!", not url_file_rc, "did not exit", "exited")
+    print_test_result("> URL_FILE command %s successfully when GITHUB_TOKEN is not set!", not url_file_rc, "did not exit", "exited")
     if not url_file_rc:
         total_correct += 1
     
@@ -99,14 +99,14 @@ def run_test_suite() -> int:
     test_suite_rc, output = test_suite.run()
 
     if test_suite_rc is False:
-        print(f"{RED}Test suite failed to run.{RESET}")
+        print(f"{RED}> Test suite failed to run.{RESET}")
         return 0
     
     total_correct = 0
     test_suite_regex = re.compile(r"(\d+)\/(\d+) test cases passed. (\d+)% line coverage achieved.", flags=re.IGNORECASE)
 
     test_suite_match = test_suite_regex.search(output)
-    print_test_result("Test suite output is %s the correct format!", test_suite_match, "in", "not in")
+    print_test_result("> Test suite output is %s the correct format!", test_suite_match, "in", "not in")
     if test_suite_match:
         total_correct += 1
     else:
@@ -116,19 +116,19 @@ def run_test_suite() -> int:
     total_tests = int(results[0][1])
     line_coverage = int(results[0][2])
 
-    print_test_result("Test suite contains %s test cases!", total_tests, "20 or more", "less than 20")
+    print_test_result("> Test suite contains %s test cases!", total_tests, "20 or more", "less than 20")
     if total_tests >= 20:
         total_correct += 1
 
     
     if line_coverage >= 80:
         total_correct += 2
-        print(f"{GREEN}> Test suite achieved 80% or greater line coverage.{RESET}")
+        print(f"{GREEN}> Test suite achieved 80% or greater line coverage. (2/2 points){RESET}")
     elif line_coverage >= 60:
         tptal_correct += 1
-        print(f"{YELLOW}> Test suite achieved 60% or greater line coverage.{RESET}")
+        print(f"{YELLOW}> Test suite achieved 60% or greater line coverage. (1/2 points){RESET}")
     else:
-        print(f"{RED}> Test suite achieved less than 60% line coverage.{RESET}")
+        print(f"{RED}> Test suite achieved less than 60% line coverage. (0/2 points) {RESET}")
     return total_correct
 
 # Suggestions:
